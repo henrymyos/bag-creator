99d74c45a4906110b0702e5fc5611d1914fac724
Henry Myos <henrymyos@gmail.com>
2026-05-06 21:31:42 -0700

    Added Firebase and login functionality

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyD4Cm8w6o59NqFiY2O_VvaXAGMHnTpfTOE",
+  authDomain:        "bag-creator.firebaseapp.com",
+  projectId:         "bag-creator",
+  storageBucket:     "bag-creator.firebasestorage.app",
+  messagingSenderId: "1081884358209",
+  appId:             "1:1081884358209:web:dd93ac769ae6456e6078c0"
+};
